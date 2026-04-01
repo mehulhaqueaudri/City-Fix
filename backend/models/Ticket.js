06c2677fabfe56c91029c69bda946dd7efd7ac72
Talha1o1 <tarifulislamtalha@gmail.com>
2026-04-02 01:53:27 +0600

    Added User and Ticket database models

---
+++ backend/models/Ticket.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const ticketSchema = new mongoose.Schema({
+  category: { type: String, required: true },
+  description: { type: String, required: true },
+  severity: { type: String, enum: ['Low', 'Medium', 'High'], required: true },
+  status: { type: String, enum: ['Open', 'In Progress', 'Resolved'], default: 'Open' },
+  location: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Ticket', ticketSchema);
