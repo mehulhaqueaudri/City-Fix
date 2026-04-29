2421a8ab68c9c90a58a723f029440518092c8969
Talha1o1 <tarifulislamtalha@gmail.com>
2026-04-29 22:47:52 +0600

    Prepared backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -31,5 +31,5 @@ app.use('/api/inventory', inventoryRoutes); // <-- UNCOMMENTED!
 app.use('/api/system', require('./routes/systemRoutes'));
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
